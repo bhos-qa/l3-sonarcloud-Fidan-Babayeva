76a8ad623ab3c154c5fa1c57942e347b8189734a
Fidan Babayeva <fidanbabayeva51@gmail.com>
2023-09-27 10:48:43 +0400

    permission changed

--- src/test/java/org/example/PermissionManagerTest.java
+++ src/test/java/org/example/PermissionManagerTest.java
@@ -10,14 +10,24 @@ public class PermissionManagerTest {
     @Test
     public void testGetRoleName() {
         // Arrange: Create a org.example.PermissionManager instance with a specific org.example.PermissionLevel
-        PermissionManager manager = new PermissionManager();
-        manager.setPermissionLevel(PermissionLevel.ADMIN);
+        PermissionManager manager_admin = new PermissionManager();
+        manager_admin.setPermissionLevel(PermissionLevel.ADMIN);
+
+        PermissionManager manager_user = new PermissionManager();
+        manager_user.setPermissionLevel(PermissionLevel.ADMIN);
+
+        PermissionManager manager_developer = new PermissionManager();
+        manager_developer.setPermissionLevel(PermissionLevel.ADMIN);
 
         // Act: Call the method you want to test
-        String roleName = manager.getRoleName();
+        String roleName_admin = manager_admin.getRoleName();
+        String roleName_user = manager_user.getRoleName();
+        String roleName_developer = manager_developer.getRoleName();
 
         // Assert: Verify the expected result
-        assertEquals("Admin", roleName);
+        assertEquals("Admin", roleName_admin);
+        assertEquals("User", roleName_user);
+        assertEquals("Developer", roleName_developer);
     }
 
     @Test
